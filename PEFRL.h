7819a9e2be6577dd0ae425319994db17a28e389f
jtonneli <45027864+jtonneli@users.noreply.github.com>
2018-11-14 07:36:29 +0100

    Update PEFRL.h

--- PEFRL.h
+++ PEFRL.h
@@ -6,9 +6,29 @@
 #include <vector>
 using namespace std;
 
+double xi = 0.1786178958448091;
+double lambda = -0.2123418310626054;
+double chi = -0.06626458266981849;
+
 // gegeven de punten op hun beginposities, uitvoeren van de PEFRL methode
 void PEFRL(vector<Body> bodies, double h, double tstart, double teind) {
+	for (double j = tstart; j < teind; j += h) {
+		for (unsigned int i = 0; i < bodies.size; ++i) {
+			Vec r = bodies.at(i).getpos();
+			Vec v = bodies.at(i).getvel();
 
+			r += xi * h*v;
+			v += 0.5*(1 - 2 * lambda)*h*calc_accel(bodies).at(i);
+			r += chi * h*v;
+			v += lambda * h*calc_accel(bodies).at(i);
+			r += (1 - 2 * (chi + xi))*h*v;
+			v += lambda * h*calc_accel(bodies).at(i);
+			r += chi * h*v;
+			v += 0.5*(1 - 2 * lambda)*h*calc_accel(bodies).at(i);
+			r += xi * h*v;
+
+		}
+	}
 }
 
 #endif PEFRL_H
